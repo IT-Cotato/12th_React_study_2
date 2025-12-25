3b3478d8e5fe980a0edd52fdb82d74dd8c699772
sxxbxxny <gyg031@naver.com>
2025-12-25 17:42:49 +0900

    react todo app 실습 업로드

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import {Component} from 'react';
+import './App.css'
+
+export default class App extends Component {
+
+    todoData = [
+        {
+            id: "1",
+            title: "공부하기",
+            completed: false,
+        },
+        {
+            id: "2",
+            title: "청소하기",
+            completed: false,
+        }
+    ]
+
+    btnStyle = {
+        color: '#fff',
+        border:'none',
+        padding: '5px 9px',
+        borderRadius: '50%',
+        cursor: 'pointer',
+        float: 'right',
+    }
+
+    getStyle = () => {
+        return {
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: 'none',
+        }
+    }
+
+    render() {
+        return (
+            <div className='container'>
+                <div className='todoBlock'>
+                    <div className='title'>
+                        <h1>할 일 목록</h1>
+                    </div>
+
+                    {
+                    this.todoData.map((data) => (
+                            <div ket={data.id} style={this.getStyle()}>
+                        <input type='checkbox' defaultChecked={false} />
+                        {data.title}
+                        <button style={this.btnStyle}>X</button>
+                    </div>
+                        )
+                    )
+                    }
+                </div>
+            </div>
+        )
+    }
+}
